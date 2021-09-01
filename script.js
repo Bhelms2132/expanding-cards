4c8bdb4e57688762552cc2eb5ffb0a2f3ddd7926
Brian Helms <bhelms2132@gmail.com>
2021-09-01 19:01:29 -0400

    Function created to remove current active card when another card is clicked

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const panels = document.querySelectorAll('.panel')
 
 //--Loops Through Panels--//
-panels.forEach((panel) => {
+panels.forEach(panel => {
     panel.addEventListener('click', () => {
         removeActiveClasses()
         panel.classList.add('active')
     })
 })
+
+//Loops through and removes active classes
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
